482052fa31c63c9b394456133ef8413b3d7ebb6f
legend80s <liuchzong@qq.com>
2025-12-30 14:44:33 +0800

    feat: click text to increment also

--- lib/main.dart
+++ lib/main.dart
@@ -104,7 +104,11 @@ class _MyHomePageState extends State<MyHomePage> {
           // wireframe for each widget.
           mainAxisAlignment: .center,
           children: [
-            const Text('You have pushed the button this many times:'),
+            OutlinedButton(
+              onPressed: _incrementCounter,
+              child: Text('1 Click me You have pushed the button this many times:'),
+
+            ),
             Text(
               '$_counter',
               style: Theme.of(context).textTheme.headlineMedium,
